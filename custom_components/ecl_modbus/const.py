01285709787f8c8227fbe9ab279fbeb2c0b13776
nichlasrohde <nichlasrohde@live.dk>
2025-12-02 12:42:52 +0100

    Update const.py

--- custom_components/ecl_modbus/const.py
+++ custom_components/ecl_modbus/const.py
@@ -1,16 +1,29 @@
 from __future__ import annotations
 
-from datetime import timedelta
-
-from homeassistant.const import Platform
-
 DOMAIN = "ecl_modbus"
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+# Konfiguration (setup via config_flow)
+CONF_BAUDRATE = "baudrate"
+CONF_SLAVE_ID = "slave_id"
 
 DEFAULT_NAME = "ECL Modbus"
 DEFAULT_BAUDRATE = 38400
 DEFAULT_SLAVE_ID = 5
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=30)
 
-CONF_SLAVE_ID = "slave_id"
-CONF_BAUDRATE = "baudrate"
+# Options: enable/disable temperatur-sensorer
+CONF_ENABLE_S1 = "enable_s1"
+CONF_ENABLE_S2 = "enable_s2"
+CONF_ENABLE_S3 = "enable_s3"
+CONF_ENABLE_S4 = "enable_s4"
+CONF_ENABLE_S5 = "enable_s5"
+CONF_ENABLE_S6 = "enable_s6"
+
+# Options: enable/disable outputs (6200+)
+CONF_ENABLE_TR1 = "enable_tr1"
+CONF_ENABLE_TR2 = "enable_tr2"
+CONF_ENABLE_R1 = "enable_r1"
+CONF_ENABLE_R2 = "enable_r2"
+CONF_ENABLE_P1_DUTY = "enable_p1_duty"
+CONF_ENABLE_P1_FREQ = "enable_p1_freq"
+CONF_ENABLE_STEPPER1 = "enable_stepper1"
+CONF_ENABLE_STEPPER2 = "enable_stepper2"
